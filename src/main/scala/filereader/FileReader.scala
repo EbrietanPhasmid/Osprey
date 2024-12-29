001923727b3371bbefde72fabae3b1ec0e1b6f02
EbrietanPhasmid <zawadiak.mrjz@gmail.com>
2024-12-29 17:33:47 +0100

    First succesful Test with file reading! Support for phoneme group file reader lacking.

--- src/main/scala/filereader/FileReader.scala
+++ src/main/scala/filereader/FileReader.scala
@@ -12,7 +12,6 @@ object FileReader {
     val lines = getLinesFromFile(filename)
     return convertToPhonemeGroupMap(lines)
   }
-
   def createSoundChangeList(): List[SoundChange] = {
     val filename = "soundchanges.txt"
     val stringList = getLinesFromFile(filename)
@@ -22,47 +21,53 @@ object FileReader {
     }
     soundChangeList
   }
-
-}
-def createWorldList(): List[String] = {
-  val filename = "wordlist.txt"
-  getLinesFromFile(filename)
-}
-def getLinesFromFile(filename: String): List[String] = {
-  val RESOURCE_DIRECTORY: String = "~/Desk/Osprey/resources/"
-  val fileDirectory: String = s"$RESOURCE_DIRECTORY$filename"
-  val file = Source.fromFile(fileDirectory)
-  val lines = file.getLines
-  file.close
-  lines.toList
-}
-def convertToPhonemeGroupMap(inputList: List[String]): Map[Char, String] = {
-  return Map('P' -> "[k,t,sr")
-}
-
-def convertToSoundChange(inputUntrimmed: String): Option[SoundChange] = {
-  val ANY_CHARS = raw"[\u0000-\uFFFF]*"
-  val CONVERSION_SYMBOL = raw">"
-  val CONDITION_SYMBOL = raw"/"
-  val TARGET_SYMBOL = raw"_"
-  def basicSoundChange(input: String): SoundChange = {
-    val basicPattern = raw"($ANY_CHARS)>($ANY_CHARS)".r
-    input match {
-      case basicPattern(target, output) => return SoundChange(target, output)
+  def createWordList(): List[String] = {
+    val filename = "wordlist.txt"
+    getLinesFromFile(filename)
+  }
+  def getLinesFromFile(filename: String): List[String] = {
+    val RESOURCE_DIRECTORY: String =
+      "/Users/marcinzawadiak/Desk/Osprey/resources/"
+    val fileDirectory: String = s"$RESOURCE_DIRECTORY$filename"
+    Try {
+      val file = Source.fromFile(fileDirectory)
+      try file.getLines.toList
+      finally file.close
+    } match {
+      case Success(lines) => lines
+      case Failure(exception) =>
+        println(s"Error when reading file $filename: ${exception.getMessage}")
+        List.empty
     }
   }
-  def quaternarySoundChange(input: String): SoundChange = {
-    val pattern = raw"($ANY_CHARS)>($ANY_CHARS)/($ANY_CHARS)_($ANY_CHARS)".r
-    input match {
-      case pattern(target, output, leftCondition, rightCondition) =>
-        return SoundChange(target, output, leftCondition, rightCondition)
-    }
+  def convertToPhonemeGroupMap(inputList: List[String]): Map[Char, String] = {
+    return Map('P' -> "[k,t,sr")
   }
-  val input = inputUntrimmed.trim
-  if !input.contains(CONVERSION_SYMBOL) then return None
-  if !input.contains(CONDITION_SYMBOL) then
-    return Some { basicSoundChange(input) }
-  if input.filter(_ == '>').size > 1 || input.filter(_ == '/').size > 1 then
-    return None
-  return Some(quaternarySoundChange(input))
+
+  def convertToSoundChange(inputUntrimmed: String): Option[SoundChange] = {
+    val ANY_CHARS = raw"[\u0000-\uFFFF]*"
+    val CONVERSION_SYMBOL = raw">"
+    val CONDITION_SYMBOL = raw"/"
+    val TARGET_SYMBOL = raw"_"
+    def basicSoundChange(input: String): SoundChange = {
+      val basicPattern = raw"($ANY_CHARS)>($ANY_CHARS)".r
+      input match {
+        case basicPattern(target, output) => return SoundChange(target, output)
+      }
+    }
+    def quaternarySoundChange(input: String): SoundChange = {
+      val pattern = raw"($ANY_CHARS)>($ANY_CHARS)/($ANY_CHARS)_($ANY_CHARS)".r
+      input match {
+        case pattern(target, output, leftCondition, rightCondition) =>
+          return SoundChange(target, output, leftCondition, rightCondition)
+      }
+    }
+    val input = inputUntrimmed.filterNot(_.isWhitespace)
+    if !input.contains(CONVERSION_SYMBOL) then return None
+    if !input.contains(CONDITION_SYMBOL) then
+      return Some { basicSoundChange(input) }
+    if input.filter(_ == '>').size > 1 || input.filter(_ == '/').size > 1 then
+      return None
+    return Some(quaternarySoundChange(input))
+  }
 }
--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -1,13 +1,18 @@
 package com.osprey.app
 package main
 
-import soundchange.*, os.*
+import soundchange.*, os.*, filereader.*
 
 object Main extends App {
-  println(SoundChange("a", "e", "aP", "")("amasata"))
-
-  def convertSoundChangeSeqToConjugate(
-      soundChangeSeq: Seq[SoundChange]
+  val wordList = FileReader.createWordList()
+  val soundChangeList = FileReader.createSoundChangeList()
+  wordList.foreach(println)
+  soundChangeList.foreach(println)
+  val conjugate = convertSoundChangeListToConjugate(soundChangeList)
+  val newWords = wordList.map(conjugate)
+  newWords.foreach(println)
+  def convertSoundChangeListToConjugate(
+      soundChangeSeq: List[SoundChange]
   ): (String) => String =
     soundChangeSeq.map(_.apply).reduce((f1, f2) => f1.andThen(f2))
 }
